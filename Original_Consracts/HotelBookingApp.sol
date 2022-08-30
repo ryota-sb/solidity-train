28cfb98b69cc07675cdb117d8da25f80c67cc415
Remix Workspace <>
2022-08-31 03:38:47 +0900

    予約システム

--- Original_Consracts/HotelBookingApp.sol
+++ Original_Consracts/HotelBookingApp.sol
@@ -1,3 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract HotelBooking {
+    // ホテルのオーナー
+    address payable public owner;
+
+    // 空き室か満室の状態
+    enum Statuses { Vacant, Occupied }
+    Statuses public currentStatuses;
+
+    // 初期化関数
+    constructor() {
+        owner = payable(msg.sender);
+        currentStatuses = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant {
+        // 満室かどうかを判定
+        require(currentStatuses == Statuses.Vacant, "already ocuppied");
+        _;
+    }
+
+    modifier costs(uint _amount) {
+        // お金が足りるか
+        require(msg.value >= _amount, "not enough ether");
+        _;
+    }
+
+    // 予約関数
+    function booking() public payable onlyWhileVacant costs(2 ether){
+        // 満室状態にする
+        currentStatuses = Statuses.Occupied;
+        // オーナーにお金を送金する
+        owner.transfer(msg.value);
+    }
+}
